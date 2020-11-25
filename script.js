cb7b9c76d26c578abbf5a444f25603a67bc2c70f
yann tyb <yanntyb.lbc@gmail.com>
2020-11-25 13:59:25 +0100

    z

---
+++ script.js
@@ -0,0 +1,27 @@
+let material = prompt("material ?")
+let phrase = "j'utilise "
+
+
+switch (material) {
+    case "souris":
+        alert(phrase + material);
+        break;
+    case "clavier":
+        alert(phrase + material);
+        break;
+    case "ordinateur":
+        alert(phrase + material);
+        break;
+    case "tapis":
+        alert(phrase + material);
+        break;
+    case "imprimante":
+        alert(phrase + material);
+        break;
+    case "ondulateur":
+        alert(phrase + material);
+        break;
+    default:
+        alert("non");
+        break;
+}
